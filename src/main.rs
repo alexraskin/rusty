1902bbe8da07a2987b2ca79275f031128ccb46a4
alexraskin <alexraskin@fastmail.fm>
2022-02-10 13:37:39 -0700

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,3 @@
-//! Requires the 'framework' feature flag be enabled in your project's
-//! `Cargo.toml`.
-//!
-//! This can be enabled by specifying the feature in the dependency section:
-//!
-//! ```toml
-//! [dependencies.serenity]
-//! git = "https://github.com/serenity-rs/serenity.git"
-//! features = ["framework", "standard_framework"]
-//! ```
 mod commands;
 
 use std::{collections::HashSet, env, sync::Arc};
@@ -48,21 +38,14 @@ struct General;
 
 #[tokio::main]
 async fn main() {
-    // This will load the environment variables located at `./.env`, relative to
-    // the CWD. See `./.env.example` for an example on how to structure this.
     dotenv::dotenv().expect("Failed to load .env file");
-
-    // Initialize the logger to use environment variables.
-    //
-    // In this case, a good default is setting the environment variable
-    // `RUST_LOG` to `debug`.
+.
     tracing_subscriber::fmt::init();
 
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
     let http = Http::new_with_token(&token);
 
-    // We will fetch your bot's owners and id
     let (owners, _bot_id) = match http.get_current_application_info().await {
         Ok(info) => {
             let mut owners = HashSet::new();
@@ -73,7 +56,6 @@ async fn main() {
         Err(why) => panic!("Could not access application info: {:?}", why),
     };
 
-    // Create the framework
     let framework =
         StandardFramework::new().configure(|c| c.owners(owners).prefix("&")).group(&GENERAL_GROUP);
 
